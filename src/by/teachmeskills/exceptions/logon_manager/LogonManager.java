0c997dc220cd812de6213a66432c24335b23a097
PavelRadzevich <pavel.radzevich@gmail.com>
2023-01-16 13:47:32 +0300

    Test1.

--- src/by/teachmeskills/exceptions/logon_manager/LogonManager.java
+++ src/by/teachmeskills/exceptions/logon_manager/LogonManager.java
@@ -7,15 +7,16 @@ public final class LogonManager {
     private String password;
     private String confirmPassword;
 
-    static boolean getLogin(String login, String password, String confirmPassword) {
+    static boolean getLogin(String login, String password, String confirmPassword)
+            throws WrongLoginException, WrongPasswordException {
         boolean result = false;
         if (login.length() >= 20 || Pattern.matches("\\s+", login)) {
-            //throw  WrongLoginException();
+            throw new WrongLoginException("Wrong Login!");
         }
         if (password.length() >= 20
                 || Pattern.matches("\\s+", password)
                 || (!password.equals(confirmPassword))) {
-            //throw  WrongPasswordException();
+            throw new WrongPasswordException("Wrong Password!");
         }
         return result;
     }
---
+++ src/by/teachmeskills/exceptions/logon_manager/WrongLoginException.java
@@ -0,0 +1,9 @@
+package by.teachmeskills.exceptions.logon_manager;
+
+public class WrongLoginException extends Exception {
+    public WrongLoginException(String message) {
+        super(message);
+    }
+
+
+}
