30051d7c26116ebbd25040827966de9f6cecedfe
PavelRadzevich <pavel.radzevich@gmail.com>
2023-01-22 03:47:32 +0300

    Test1

--- src/by/teachmeskills/strings/document_processing/DocumentProcessing.java
+++ src/by/teachmeskills/strings/document_processing/DocumentProcessing.java
@@ -1,13 +1,73 @@
 package by.teachmeskills.strings.document_processing;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class DocumentProcessing {
+    public static String get4digit(String string) {
+        Pattern pattern = Pattern.compile("\\d{4}");
+        Matcher mat = pattern.matcher(string);
+        String result = "";
+        while (mat.find()) {
+            result += mat.group() + " ";
+        }
+        return result;
+    }
 
-    public DocumentProcessing(){}
+    public static String setCharsToAsterisks(String string) {
+        String repString = "***";
+        Pattern pattern = Pattern.compile("[a-z]{3}");
+        Matcher mat = pattern.matcher(string);
+        return mat.replaceAll(repString);
+    }
 
-    static public void get4digit(){
+    public static String getAllCharsSet1(String string) {
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        Matcher mat = pattern.matcher(string);
+        String result = "";
+        while (mat.find()) {
+            result += mat.group().toLowerCase() + "/";
+        }
+        return result;
+    }
 
+    public static String getAllCharsSet2(String string) {
+        Pattern pattern = Pattern.compile("[a-zA-Z]+");
+        Matcher mat = pattern.matcher(string);
+        StringBuilder stringB = new StringBuilder();
+        stringB.append("\"Letters:");
+        while (mat.find()) {
+            stringB.append(mat.group().toLowerCase()).append("/");
+        }
+        stringB.replace(stringB.length() - 1, stringB.length(), "\"");
+        return stringB.toString();
+    }
+
+    public static void checkStrForSeq(String string, String seq) {
+        //if (Pattern.compile(seq.toLowerCase()).matcher(string).find()) {
+        if (string.contains(seq.toLowerCase())) {
+            System.out.println("The name " + string + " contains sequences: " + seq);
+        } else {
+            System.out.println("The name " + string + " doesn't contains sequences: " + seq);
+        }
+    }
+
+    public static void checkBeginningStr(String string, String seq) {
+        //if (Pattern.compile("^" + seq).matcher(string).find()) {
+        if (string.startsWith(seq)) {
+            System.out.println("The name " + string + " starts with a sequence: " + seq);
+        } else {
+            System.out.println("The name: " + string + " doesn't starts with a sequence: " + seq);
+        }
 
     }
 
-
+    public static void checkEndingStr(String string, String seq) {
+        //if (Pattern.compile(seq + "$").matcher(string).find()) {
+        if (string.endsWith(seq)) {
+            System.out.println("The name " + string + " ends with a sequence: " + seq);
+        } else {
+            System.out.println("The name " + string + " doesn't ends with a sequence: " + seq);
+        }
+    }
 }
--- src/by/teachmeskills/strings/document_processing/DocumentProcessingDemo.java
+++ src/by/teachmeskills/strings/document_processing/DocumentProcessingDemo.java
@@ -16,8 +16,7 @@ package by.teachmeskills.strings.document_processing;
          (входным параметром) будут принимать вводимую на вход программы строку.
  */
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import static by.teachmeskills.strings.document_processing.DocumentProcessing.*;
 
 public class DocumentProcessingDemo {
     public static void main(String[] args) {
@@ -25,74 +24,9 @@ public class DocumentProcessingDemo {
         System.out.println(get4digit(testNameDocument));
         System.out.println(setCharsToAsterisks(testNameDocument));
         System.out.println(getAllCharsSet1(testNameDocument));
-        checkStrForSeq(testNameDocument, "ABC");
-        checkBeginningStr(testNameDocument, "555");
-        checkEndingStr(testNameDocument, "1a2b");
+        System.out.println(getAllCharsSet2(testNameDocument));
+        checkStrForSeq(testNameDocument, "ABD");
+        checkBeginningStr(testNameDocument, "556");
+        checkEndingStr(testNameDocument, "1a2c");
     }
-
-
-    public static String get4digit(String string) {
-        Pattern pattern = Pattern.compile("\\d{4}");
-        Matcher mat = pattern.matcher(string);
-        String result = "";
-        while (mat.find()) {
-            result += mat.group() + " ";
-        }
-        return result;
-    }
-
-    public static String setCharsToAsterisks(String string) {
-        String repString = "***";
-        Pattern pattern = Pattern.compile("[a-z]{3}");
-        Matcher mat = pattern.matcher(string);
-        return mat.replaceAll(repString);
-    }
-
-    public static String getAllCharsSet1(String string) {
-        Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        Matcher mat = pattern.matcher(string);
-        String result = "";
-        while (mat.find()) {
-            result += mat.group().toLowerCase() + "/";
-        }
-        return result;
-    }
-
-    public static String getAllCharsSet2(String string) {
-        Pattern pattern = Pattern.compile("[a-zA-Z]+");
-        Matcher mat = pattern.matcher(string);
-        String result = "";
-        while (mat.find()) {
-            result += mat.group().toLowerCase() + "/";
-        }
-        return result;
-    }
-
-    public static void checkStrForSeq(String string, String seq) {
-        if (Pattern.compile(seq.toLowerCase()).matcher(string).find()) {
-            System.out.println("The name " + string + " contains sequences: " + seq);
-        } else {
-            System.out.println("The name " + string + " doesn't contains sequences: " + seq);
-        }
-    }
-
-    public static void checkBeginningStr(String string, String seq) {
-        if (Pattern.compile("^" + seq).matcher(string).find()) {
-            //if   (string.startsWith(seq)){
-            System.out.println("The name " + string + " starts with a sequence: " + seq);
-        } else {
-            System.out.println("The name: " + string + " doesn't starts with a sequence: " + seq);
-        }
-
-    }
-
-    public static void checkEndingStr(String string, String seq) {
-        if (Pattern.compile(seq + "$").matcher(string).find()) {
-            System.out.println("The name " + string + " ends with a sequence: " + seq);
-        } else {
-            System.out.println("The name " + string + " doesn't ends with a sequence: " + seq);
-        }
-    }
-
-
 }
